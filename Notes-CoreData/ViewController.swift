990163c5c3b621b042c7eb4f3db01f7e2205596b
rustemsupayev <rustik7252@gmail.com>
2019-11-20 20:50:26 +0600

    Initial Commit

---
+++ Notes-CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes-CoreData
+//
+//  Created by Rustem Supayev on 20/11/2019.
+//  Copyright © 2019 Rustem Supayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
