ab397d362990cdb19eb612411c3eb03aa23b03d9
rustemsupayev <rustik7252@gmail.com>
2019-11-24 20:04:42 +0600

    Added delete row function

--- Notes-CoreData/AppDelegate.swift
+++ Notes-CoreData/AppDelegate.swift
@@ -13,7 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
@@ -25,6 +25,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "CoreData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                
+                fatalError("Unresolved error, \((error as NSError).userInfo)")
+            }
+        })
+        return container
+    }()
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Notes-CoreData/ViewController.swift
+++ Notes-CoreData/ViewController.swift
@@ -7,47 +7,97 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
 
-    var items: [String] = []
+    var items: [NSManagedObject] = []
     let cellId = "cellId"
+    let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
-        view.backgroundColor = .red
+        view.backgroundColor = .white
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addItem))
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        do {
+            items = try managedContext.fetch(fetchRequest)
+        } catch let err as NSError {
+            print("Failed to fetch", err)
+        }
+    }
+    
     @objc func addItem(_sender: AnyObject) {
-        let alertController = UIAlertController(title: "Add a new note", message: "Please fill up the text box bellow", preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Add", message: "please add item", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
-            guard let textField = alertController.textFields?.first, let itemsToAdd = textField.text else { return }
-            self.items.append(itemsToAdd)
+            guard let textField = alertController.textFields?.first, let itemToAdd = textField.text else { return }
+            self.save(itemToAdd)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
         alertController.addTextField(configurationHandler: nil)
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
-        present(alertController,animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
     
-
+    func save(_ itemName: String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Item", in: managedContext)
+        let item = NSManagedObject(entity: entity!, insertInto: managedContext)
+        item.setValue(itemName, forKey: "itemName")
+        
+        do {
+            try managedContext.save()
+            items.append(item)
+        } catch let err as NSError {
+            print("failed to save", err)
+        }
+        
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         let item = items[indexPath.row]
-        cell.textLabel?.text = item
+        cell.textLabel?.text = item.value(forKeyPath: "itemName") as? String
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            let managedContext = appDelegate.persistentContainer.viewContext
+            
+            managedContext.delete(self.items[indexPath.row])
+            
+            do {
+                try managedContext.save()
+                self.items.remove(at: indexPath.row)
+                self.fetchRequest
+                self.tableView.reloadData()
+            } catch {
+                print("Failed to delete")
+            }
+            
+        }
+    }
     
 }
 
